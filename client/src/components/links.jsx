549c30fc812056ff360cb8c5f2334fb38eeab662
Daniel Varcas <daniel.varcas@gmail.com>
2020-05-10 20:24:36 +0100

    Responsive layout for Links page.

--- client/src/components/links.jsx
+++ client/src/components/links.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { CardColumns, Card } from 'react-bootstrap';
 
 import PropTypes from 'prop-types';
 
@@ -7,19 +7,28 @@ function Links(props) {
   const { links } = props;
 
   return (
-    <Card.Group className="gm_links" centered itemsPerRow={3}>
+    <CardColumns className="m-3">
       {links.map(link => (
-        <Card key={link.url} href={link.url}>
-          <Card.Content>
-            {link.image && (
-              <Image floated="right" size="mini" src={link.image} />
-            )}
-            <Card.Header>{link.header}</Card.Header>
-            <Card.Description>{link.description}</Card.Description>
-          </Card.Content>
+        <Card key={link.url}>
+          <Card.Header>
+            <div className="d-flex flex-row justify-content-between">
+              <Card.Title>
+                <a href={link.url} className="stretched-link text-dark">
+                  {link.header}
+                </a>
+              </Card.Title>
+              {link.image && (
+                <img alt="" src={link.image} style={{ maxWidth: '35px' }} />
+              )}
+            </div>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>{link.description}</Card.Text>
+            <Card.Link />
+          </Card.Body>
         </Card>
       ))}
-    </Card.Group>
+    </CardColumns>
   );
 }
 
