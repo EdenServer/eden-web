5b7fbd349786b13ae7f66c4cfbe4b77511b7a264
Daniel Varcas <daniel.varcas@gmail.com>
2020-05-10 18:27:23 +0100

    Add tab functionality to Contributing.

--- client/src/components/about/contributing.jsx
+++ client/src/components/about/contributing.jsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Card, Nav } from 'react-bootstrap';
 import Setup from './setup';
 
-export default () => {
-    const [tab, setTab] = React.useState(0);
-    return (
-        <React.Fragment>
-            <Menu secondary pointing>
-                <Menu.Item active={tab === 0} onClick={() => setTab(0)}>
-                  Setting up DSP
-                </Menu.Item>
-                <Menu.Item disabled>
-                  Writing BCNMs
-                </Menu.Item>
-                <Menu.Item disabled>
-                  Eden Docs
-                </Menu.Item>
-            </Menu>
-            {tab === 0 && <Setup />}
-        </React.Fragment>
-    );
+function Contributing() {
+  const setupContent = () => <Setup />;
+  const writingBcnmsContent = () => (
+    <p>&quot;Writing BCNMs&quot; coming soon...</p>
+  );
+  const edenDocsContent = () => <p>&quot;Eden Docs&quot; coming soon...</p>;
+
+  const [activeKey, setActiveKey] = useState('setup');
+  const [activeContent, setActiveContent] = useState(setupContent);
+
+  useEffect(() => {
+    switch (activeKey) {
+      case 'setup':
+        setActiveContent(setupContent());
+        break;
+      case 'bcnms':
+        setActiveContent(writingBcnmsContent());
+        break;
+      case 'docs':
+        setActiveContent(edenDocsContent());
+        break;
+      default:
+        setActiveContent('');
+    }
+  }, [activeKey]);
+
+  return (
+    <Card bg="light" className="m-3">
+      <Card.Header>
+        <Nav
+          variant="Tabs"
+          onSelect={k => setActiveKey(k)}
+          activeKey={activeKey}
+        >
+          <Nav.Item>
+            <Nav.Link eventKey="setup" href="#setting-up-dsp">
+              Setting up DSP
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link eventKey="bcnms" href="#writing-bcnms">
+              Writing BCNMs
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link eventKey="docs" href="#eden-docs">
+              Eden Docs
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Card.Header>
+
+      <Card.Body>{activeContent}</Card.Body>
+    </Card>
+  );
 }
+
+export default Contributing;
