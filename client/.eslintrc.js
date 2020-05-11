82f563f7e84bb5b219db70940761329af4c1c284
Daniel Varcas <daniel.varcas@gmail.com>
2020-05-11 19:55:53 +0100

    Site up if api down + further eslint/prettier config setup (#2)
    * Setup eslint + prettier and install Typescript in frontend.
    
    * Get homepage to render when API down. Add api.js. Some more eslint/prettier setup.

---
+++ client/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
