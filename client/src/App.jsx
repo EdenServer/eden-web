c9c73f49fc72a804dd28810a60a9cb7f7017d2e8
Daniel Varcas <daniel.varcas@gmail.com>
2020-05-11 20:06:18 +0100

    Update readme and and introduce proptypes (#3)
    * Setup eslint + prettier and install Typescript in frontend.
    
    * Get homepage to render when API down. Add api.js. Some more eslint/prettier setup.
    
    * Install prop-types and check props in Page.
    
    * Update README.

---
+++ client/src/App.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as api from './api';
+
+import LandingPage from './components/page';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ready: false,
+      apiNoResponse: false,
+      profile: {
+        emailModalOpen: false,
+        passwordModalOpen: false,
+        updatedEmail: '',
+        updatedPassword: '',
+      },
+      config: {},
+    };
+
+    this.setContext = this.setContext.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.getConfig();
+  }
+
+  setContext(updatedContext) {
+    this.setState(state => {
+      return { ...state, ...updatedContext };
+    });
+  }
+
+  setConfig(config) {
+    this.setState({ config, ready: true });
+  }
+
+  async getConfig() {
+    await api
+      .getConfig()
+      .then(response => this.setConfig(response.data))
+      .catch(() => this.handleErrorOnGetConfig());
+  }
+
+  handleErrorOnGetConfig() {
+    this.setState({ config: {}, ready: true });
+  }
+
+  render() {
+    const { ready, config, apiNoResponse } = this.state;
+
+    return (
+      <>
+        {ready && (
+          <Router>
+            <LandingPage config={config} apiNoResponse={apiNoResponse} />
+          </Router>
+        )}
+      </>
+    );
+  }
+}
+
+export default App;
--- client/src/components/page.jsx
+++ client/src/components/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Menu from './menu';
 import Hamburger from './hamburger';
@@ -12,32 +13,106 @@ import Contact from './contact';
 import './style.css';
 
 const Page = ({ location, config }) => {
-    const [active, setActive] = React.useState(false);
+  const [active, setActive] = React.useState(false);
 
-    const changePage = (_e, data) => {
-        localStorage.getItem('page', data.to);
-        setActive(false);
-    }
+  const changePage = (_e, data) => {
+    localStorage.getItem('page', data.to);
+    setActive(false);
+  };
 
-    return (
-        <div className="gm_main">
-            <div className="gm_banner">
-                <Hamburger active={active} toggle={() => setActive(!active)} />
-                <h2 className="gm_banner_text">Eden</h2>
-            </div>
-            <Menu active={active} selection={location.pathname} onClick={changePage} />
-            <Switch>
-                <Route exact path="/install" render={props => <Install info={config.install} {...props} />} />
-                <Route exact path="/tools" render={props => <Tools {...props} />} />
-                <Route exact path="/links" render={props => <Links links={config.links} {...props} />} />
-                <Route exact path="/rules" render={props => <Rules list={config.rules} {...props} />} />
-                <Route exact path="/about" render={About} />
-                <Route exact path="/home" render={props => <Home posts={config.posts} {...props} />} />
-                <Route exact path="/contact" render={() => <Contact />} />
-                <Redirect from="/" to={localStorage.getItem('page') || '/home' } />
-            </Switch>
+  return (
+    <div className="gm_main">
+      <div className="gm_banner">
+        <Hamburger active={active} toggle={() => setActive(!active)} />
+        <h2 className="gm_banner_text">Eden</h2>
+      </div>
+      {config && Object.keys(config).length === 0 && (
+        <div className="alert alert-warning m-0 text-center">
+          <span>
+            Website tools are currently down. Please check again later.
+          </span>
         </div>
-    );
+      )}
+      <Menu
+        active={active}
+        selection={location.pathname}
+        onClick={changePage}
+      />
+      <Switch>
+        <Route
+          exact
+          path="/install"
+          render={props => (
+            <Install info={config.install ? config.install : null} {...props} />
+          )}
+        />
+        <Route exact path="/tools" render={props => <Tools {...props} />} />
+        <Route
+          exact
+          path="/links"
+          render={props => <Links links={config.links} {...props} />}
+        />
+        <Route
+          exact
+          path="/rules"
+          render={props => <Rules list={config.rules} {...props} />}
+        />
+        <Route exact path="/about" render={About} />
+        <Route
+          exact
+          path="/home"
+          render={props => (
+            <Home posts={config.posts ? config.posts : null} {...props} />
+          )}
+        />
+        <Route exact path="/contact" render={() => <Contact />} />
+        <Redirect from="/" to={localStorage.getItem('page') || '/home'} />
+      </Switch>
+    </div>
+  );
+};
+
+Page.propTypes = {
+  location: PropTypes.shape({
+    hash: PropTypes.string,
+    key: PropTypes.string,
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+    // state: ???
+  }).isRequired,
+
+  config: PropTypes.shape({
+    install: PropTypes.shape({
+      bootloader: PropTypes.string.isRequired,
+      discord: PropTypes.string.isRequired,
+      source1: PropTypes.string.isRequired,
+      source2: PropTypes.string.isRequired,
+    }).isRequired,
+    links: PropTypes.arrayOf(
+      PropTypes.shape({
+        description: PropTypes.string,
+        header: PropTypes.string,
+        image: PropTypes.string,
+        url: PropTypes.string,
+      })
+    ).isRequired,
+    posts: PropTypes.arrayOf(
+      PropTypes.shape({
+        author: PropTypes.string,
+        data: PropTypes.string,
+        message: PropTypes.string,
+        title: PropTypes.string,
+      })
+    ).isRequired,
+    rules: PropTypes.shape({
+      allowed: PropTypes.arrayOf(PropTypes.string),
+      disallowed: PropTypes.arrayOf(PropTypes.string),
+      discord: PropTypes.arrayOf(PropTypes.string),
+      rules: PropTypes.arrayOf(PropTypes.string),
+      terms: PropTypes.arrayOf(PropTypes.string),
+      yells: PropTypes.arrayOf(PropTypes.string),
+    }),
+  }).isRequired,
 };
 
 export default withRouter(Page);
