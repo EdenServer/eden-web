f21fdc1cab993be99f9183f95768d1bfda71be6e
Daniel Varcas <daniel.varcas@gmail.com>
2020-05-16 10:17:45 +0100

    Show less card columns on tablet view

--- client/src/components/links.jsx
+++ client/src/components/links.jsx
@@ -10,7 +10,7 @@ function Links(props) {
     <Container fluid className="my-3">
       <Row>
         {links.map(link => (
-          <Col xs={12} md={6} lg={4} xl={3} className="my-2 my-md-3">
+          <Col xs={12} md={6} xl={3} className="my-2 my-md-3">
             <Card key={link.url} className="h-100 link-card">
               <Card.Header>
                 <div className="d-flex flex-row justify-content-between">
