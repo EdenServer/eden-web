d45329208b8697415bff6ed1d28d287621a2dcf1
Daniel Varcas <daniel.varcas@gmail.com>
2020-05-21 21:56:07 +0100

    Remove footer padding and vertically align text.

--- client/src/components/SiteFooter/SiteFooter.jsx
+++ client/src/components/SiteFooter/SiteFooter.jsx
@@ -3,14 +3,14 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const SiteFooter = () => {
   return (
-    <Container fluid className="w-100 bg-dark text-light text-center py-3">
+    <Container fluid className="w-100 bg-dark text-light text-center">
       <Row>
-        <Col xs={12} lg>
+        <Col xs={12} lg className="my-auto">
           <p className="h4 p-3">
             <b>EDEN</b> | Classic FFXI Server
           </p>
         </Col>
-        <Col xs={12} lg>
+        <Col xs={12} lg className="my-auto">
           <p className="text-light">
             <small>
               All FINAL FANTASY® XI content and images © 2002-2020{' '}
