b6d9d6cde4aea98a2c1a1cf338f4b510f4951eee
Brian Kesecker <45717129+bkesecker@users.noreply.github.com>
2020-06-05 11:53:38 -0400

    Missed a Discord invite reference (can this be centralized?).

--- api/v1/config.js
+++ api/v1/config.js
@@ -171,7 +171,7 @@ module.exports = {
   },
   links: [
     {
-      url: 'https://discord.gg/MWtDws8',
+      url: 'https://discord.gg/N49F7wJ', // Taken from #information Discord channel on 6/5/2020.
       image:
         'https://vignette.wikia.nocookie.net/ffxi/images/0/08/Flag.jpg/revision/latest?cb=20060211183144',
       header: 'Eden Discord',
