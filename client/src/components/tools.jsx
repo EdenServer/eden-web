d6921ab542e2ba6e295493bd6dab81c1f3227866
Daniel Varcas <daniel.varcas@gmail.com>
2020-06-06 18:12:42 +0100

    Fixes #78 - Make tabs responsive on tools. Change primary colour so links aren't default bootstrap blue.

--- client/src/components/tools.jsx
+++ client/src/components/tools.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
-
 import { createHistory, navigate } from '@reach/router';
 import Accounts from './accounts';
 import Itemsearch from './tools/itemsearch';
 import Playersearch from './tools/playersearch';
 import OnlineList from './tools/OnlineList';
 import YellTab from './tools/YellTab';
+import { Nav } from 'react-bootstrap';
 
 const Tools = () => {
   const history = createHistory(window);
@@ -30,31 +29,58 @@ const Tools = () => {
 
   return (
     <div className="gm_tools">
-      <div className="gm_tools-content">
-        <Menu pointing>
-          <Menu.Item
-            disabled
-            active={selected === 'account'}
-            onClick={updateTab('account')}
-          >
-            User Management
-          </Menu.Item>
-          <Menu.Item
-            active={selected === 'online'}
-            onClick={updateTab('online')}
-          >
-            Who's Online
-          </Menu.Item>
-          <Menu.Item active={selected === 'items'} onClick={updateTab('items')}>
-            Item Search
-          </Menu.Item>
-          <Menu.Item active={selected === 'chars'} onClick={updateTab('chars')}>
-            Player Search
-          </Menu.Item>
-          <Menu.Item active={selected === 'yells'} onClick={updateTab('yells')}>
-            Yells
-          </Menu.Item>
-        </Menu>
+      <div className="gm_tools-content bg-light py-3">
+        <Nav fill variant="tabs">
+          <Nav.Item>
+            <Nav.Link
+              disabled
+              active={selected === 'account'}
+              onClick={updateTab('account')}
+              eventKey="1"
+            >
+              User Management
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link
+              active={selected === 'online'}
+              onClick={updateTab('online')}
+              eventKey="2"
+            >
+              Who's Online
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              active={selected === 'items'}
+              onClick={updateTab('items')}
+              eventKey="3"
+            >
+              Item Search
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link
+              active={selected === 'chars'}
+              onClick={updateTab('chars')}
+              eventKey="4"
+            >
+              Player Search
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link
+              active={selected === 'yells'}
+              onClick={updateTab('yells')}
+              eventKey="5"
+            >
+              Yells
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
         {/* {selected === 'account' && <Accounts />} */}
         {selected === 'online' && <OnlineList />}
         {(item || selected === 'items') && (
