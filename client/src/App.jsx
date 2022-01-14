3e1fed128e2b83b733ab1fc7e66cab0806af874d
Evan Edwards <evan@ejedev.com>
2022-01-13 17:43:31 -0800

    Added dark mode toggle.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNewspaper, faDesktop, faWrench, faLink, faGavel, faInfoCircle, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, faDesktop, faWrench, faLink, faGavel, faInfoCircle, faHome, faAdjust } from '@fortawesome/free-solid-svg-icons';
 import * as api from './api';
+import theme from './themeUtil';
 
 import LandingPage from './components/page';
 import SiteNavbar from './components/SiteNavbar/SiteNavbar';
@@ -51,49 +52,65 @@ class App extends Component {
   }
 
   render() {
+    theme.update();
     const { ready, config, apiNoResponse } = this.state;
     const links = [
       {
         key: 'site-link-home',
+        type: 'link',
         to: '/',
         text: 'Home',
         icon: <FontAwesomeIcon icon={faHome} />,
       },
       {
         key: 'site-link-news',
+        type: 'link',
         to: '/posts',
         text: 'Posts',
         icon: <FontAwesomeIcon icon={faNewspaper} />,
       },
       {
         key: 'site-link-install',
+        type: 'link',
         to: '/install',
         text: 'Install',
         icon: <FontAwesomeIcon icon={faDesktop} />,
       },
       {
         key: 'site-link-tools',
+        type: 'link',
         to: '/tools',
         text: 'Tools',
         icon: <FontAwesomeIcon icon={faWrench} />,
       },
       {
         key: 'site-link-links',
+        type: 'link',
         to: '/links',
         text: 'Links',
         icon: <FontAwesomeIcon icon={faLink} />,
       },
       {
         key: 'site-links-rules',
+        type: 'link',
         to: '/rules',
         text: 'Rules',
         icon: <FontAwesomeIcon icon={faGavel} />,
       },
       {
         key: 'site-links-about',
+        type: 'link',
         to: '/about',
         text: 'About',
         icon: <FontAwesomeIcon icon={faInfoCircle} />,
+      },
+      {
+        key: 'site-links-darkmode',
+        type: 'function',
+        to: '/',
+        text: '',
+        icon: <FontAwesomeIcon icon={faAdjust} />,
+        func: theme.change,
       },
       // {
       //   key: 'site-links-contact',
