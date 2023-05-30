53f4eb0e3e5d51baa4f5779533f7ae017f78687e
59blargedy <59blargedy@gmail.com>
2023-05-29 22:14:50 -0400

    formatting

--- client/src/components/install.jsx
+++ client/src/components/install.jsx
@@ -36,8 +36,8 @@ function install(props) {
         <Col>
           <InfoDisplay title="Account Registration">
             <Card.Text>
-              The bootloader (black login screen) supports registering an account. If you are prompted for a registration code, one can be obtained from
-              players who have accumulated 120 hours (5 days) of playtime and who's current job is level 15 or greater. Make sure you have Eden completely installed
+              The bootloader (black login screen) supports registering an account. If you are prompted for a registration code, one can be obtained from players
+              who have accumulated 120 hours (5 days) of playtime and who's current job is level 15 or greater. Make sure you have Eden completely installed
               before requesting one. The easiest way to get a registration code is to join&nbsp;
               <a href={info.discord} rel="noopener noreferrer" target="_blank">
                 our Discord
