abf98d3e6baa69474769aba17373d90274c86a5d
59blargedy <59blargedy@gmail.com>
2023-12-08 16:55:55 -0500

    prettier round 2

--- api/v1/config.js
+++ api/v1/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  rules : {
-    terms : {
-      updated : '5/22/2020',
-      list : [
+  rules: {
+    terms: {
+      updated: '5/22/2020',
+      list: [
         'You will be watched from time to time by the active/hidden GM team that patrols and responds to reports for cheating.',
         'Like most online games, Eden will collect and store data from you such as IP addresses, MAC addresses, and other unique device identifiers to help curb cheating.',
         'You will follow the server rules listed below. ALL rules may be met with punishment including up to a ban on all Eden content.',
@@ -13,9 +13,9 @@ module.exports = {
         'You acknowledge that Eden has no ownership over or affiliation with any of the content, images, registered trademarks or any other rights reserved by SQUARE ENIX CO., LTD.',
       ],
     },
-    rules : {
-      updated : '3/25/2023',
-      list : [
+    rules: {
+      updated: '3/25/2023',
+      list: [
         'Using a non-Eden bootloader--including using a modified version of the Eden bootloader--is NOT allowed. The bootloader must be downloaded from the official site or #annoucements channel in our official Discord otherwise there are no guarantees it is official.',
         'If something feels like it is broken, either not like it was on era retail or a potential exploit or too overpowered (read: if something seems like it was not intentional from the development team) report it. Knowingly taking advantage of these activities is NOT allowed.',
         'Each player is only allowed to have 2 characters logged in and active, with an additional third character confined to a city and idle. This character may only be used to bazaar or mule items while your other characters are logged in. (This will not be locked down by IP, but it will be actively patrolled.) Tri-boxing 3 or more active characters is NOT allowed.',
@@ -36,97 +36,90 @@ module.exports = {
         'Use of, or advertisement of, any cheat program/addon or a non-approved addon/program on Eden or its media is NOT allowed. There will be an allowed list and a non-allowed list. Those not in the approved list are discouraged from use as you may be penalized for using them. Use at your own risk. Those that are similar to a forbidden program are most likely forbidden as well.',
         'GM interaction is limited here but if a GM messages you in game and you are active, respond within a reasonable time. Ignoring a GM is likely to get you jailed as we will likely think you are afk botting.',
       ],
-      violations : [
+      violations: [
         {
-          rule : 'Using Bots to include Fish Botting and Claim Botting',
-          consequence : 'Banned',
+          rule: 'Using Bots to include Fish Botting and Claim Botting',
+          consequence: 'Banned',
         },
-        {rule : 'Pos Hacking / Speed Hacking', consequence : 'Banned'},
+        { rule: 'Pos Hacking / Speed Hacking', consequence: 'Banned' },
         {
-          rule : 'Harassment',
-          consequence : 'Staff Discretion',
-        },
-        {
-          rule : 'Navmesh Abuse',
-          consequence : 'Staff Discretion',
+          rule: 'Harassment',
+          consequence: 'Staff Discretion',
         },
         {
-          rule : 'Intentional MPK',
-          consequence : 'Staff Discretion',
+          rule: 'Navmesh Abuse',
+          consequence: 'Staff Discretion',
         },
         {
-          rule : 'RMT (Buying and Selling)',
-          consequence : 'Banned (Buyer and Seller)',
+          rule: 'Intentional MPK',
+          consequence: 'Staff Discretion',
         },
         {
-          rule : 'Too many characters online',
-          consequence : 'Staff Discretion',
+          rule: 'RMT (Buying and Selling)',
+          consequence: 'Banned (Buyer and Seller)',
         },
         {
-          rule : 'Gardening on more than 3 characters',
-          consequence : 'Staff Discretion',
+          rule: 'Too many characters online',
+          consequence: 'Staff Discretion',
         },
         {
-          rule : 'Severe Exploits',
-          consequence : 'Banned',
+          rule: 'Gardening on more than 3 characters',
+          consequence: 'Staff Discretion',
         },
         {
-          rule : 'Inappropriate Character Names',
-          consequence : 'Staff Discretion',
-        },
-      ],
-    },
-    software : {
-      updated : '3/25/2023',
-      list : [
-        {item : 'Combat Parsers', allowed : true},
-        {item : 'GearLock', windower : 'Dressup', allowed : true},
-        {item : 'TPParty', windower : 'TParty', allowed : true},
-        {item : 'WatchEXP', windower : 'Pointwatch', allowed : true},
-        {item : 'FPS', windower : 'Config', allowed : true},
-        {item : 'Duration', allowed : true},
-        {item : 'HideConsole', allowed : true},
-        {item : 'Recast', windower : 'Timers', allowed : true},
-        {item : 'Timestamp', allowed : true},
-        {
-          item :
-              'Shorthand (only for actions on players -- NOT allowed for claiming)',
-          allowed : true,
+          rule: 'Severe Exploits',
+          consequence: 'Banned',
         },
         {
-          item :
-              'Renamer (only using the lists we provide -- NOT for custom naming of mobs)',
-          allowed : true,
-        },
-        {
-          item :
-              'Any modified bootloader or a bootloader downloaded from any source other than edenxi.com',
-        },
-        {item : 'AuctionHelper / Auction House assistants'},
-        {item : 'Multisend / Servo'},
-        {
-          item :
-              'Scripts ran unattended. (If a GM speaks to you and you do not reply within a certain time frame, you may be jailed.)',
-        },
-        {item : 'Shorthand (when used to claim)'},
-        {item : 'LightLuggage'},
-        {
-          item :
-              'Fishing/exp/healing bots or any other bots that automate player actions',
-        },
-        {
-          item :
-              'Claim tools or character enhancement tools like flee or warp hacks.',
-        },
-        {
-          item :
-              "WatchDog (or anything that gives you abilities you shouldn't have access to)",
+          rule: 'Inappropriate Character Names',
+          consequence: 'Staff Discretion',
         },
       ],
     },
-    yells : {
-      updated : '7/12/2020',
-      list : [
+    software: {
+      updated: '3/25/2023',
+      list: [
+        { item: 'Combat Parsers', allowed: true },
+        { item: 'GearLock', windower: 'Dressup', allowed: true },
+        { item: 'TPParty', windower: 'TParty', allowed: true },
+        { item: 'WatchEXP', windower: 'Pointwatch', allowed: true },
+        { item: 'FPS', windower: 'Config', allowed: true },
+        { item: 'Duration', allowed: true },
+        { item: 'HideConsole', allowed: true },
+        { item: 'Recast', windower: 'Timers', allowed: true },
+        { item: 'Timestamp', allowed: true },
+        {
+          item: 'Shorthand (only for actions on players -- NOT allowed for claiming)',
+          allowed: true,
+        },
+        {
+          item: 'Renamer (only using the lists we provide -- NOT for custom naming of mobs)',
+          allowed: true,
+        },
+        {
+          item: 'Any modified bootloader or a bootloader downloaded from any source other than edenxi.com',
+        },
+        { item: 'AuctionHelper / Auction House assistants' },
+        { item: 'Multisend / Servo' },
+        {
+          item: 'Scripts ran unattended. (If a GM speaks to you and you do not reply within a certain time frame, you may be jailed.)',
+        },
+        { item: 'Shorthand (when used to claim)' },
+        { item: 'LightLuggage' },
+        {
+          item: 'Fishing/exp/healing bots or any other bots that automate player actions',
+        },
+        {
+          item: 'Claim tools or character enhancement tools like flee or warp hacks.',
+        },
+        {
+          item: "WatchDog (or anything that gives you abilities you shouldn't have access to)",
+        },
+      ],
+    },
+    yells: {
+      updated: '7/12/2020',
+      list: [
         'Yell is a global chat and has a 15 minute cooldown period. Shout is limited to a single zone with a large radius and has a 5 minute cooldown period, except in some instanced zones.',
         'Zoning and logging off do not reset these cooldowns. New characters will have yell and shout cooldowns applied during the opening cutscene. Do not expect these limitations to change, as they are applied to prevent abuse.',
         'Do not yell or shout profanity, hate messages, or any harassing statements. Violating this rule may result in mutes and/or an account ban depending on severity.',
@@ -135,15 +128,13 @@ module.exports = {
         'Adding on irrelevant banter not related to the purpose of a yell may result in a mute. Example: “WTS pebbles also... player XYZ sux!”',
       ],
     },
-    search : {
-      updated : '12/14/2020',
-      list : [
-        'Do not use profanity, include hate messages or any harassing statements. These are public facing channels.'
-      ],
+    search: {
+      updated: '12/14/2020',
+      list: ['Do not use profanity, include hate messages or any harassing statements. These are public facing channels.'],
     },
-    discord : {
-      updated : '5/22/2020',
-      list : [
+    discord: {
+      updated: '5/22/2020',
+      list: [
         "Please be respectful to each other. Consider this phrase when typing, treat others as you would want to be treated. You wouldn't want someone yelling at you or belittling you so you don't need to do it to another person.",
         'Please keep the cursing at a minimum. Some people prefer not to read constant cursing. We know sometimes a word or two will slip out in the heat of the moment and that is ok but if it becomes a habit we will go to you in private about it.',
         'Do not post nudity of any kind.',
@@ -153,99 +144,89 @@ module.exports = {
       ],
     },
   },
-  links : [
+  links: [
     {
-      url : 'https://discord.gg/S3EAWr2Jec',
-      image :
-          'https://vignette.wikia.nocookie.net/ffxi/images/0/08/Flag.jpg/revision/latest?cb=20060211183144',
-      header : 'Eden Discord',
-      description :
-          'Join us on Discord for technical support, up-to-date announcements, community events, memes, live chat with staff, and more.',
+      url: 'https://discord.gg/S3EAWr2Jec',
+      image: 'https://vignette.wikia.nocookie.net/ffxi/images/0/08/Flag.jpg/revision/latest?cb=20060211183144',
+      header: 'Eden Discord',
+      description: 'Join us on Discord for technical support, up-to-date announcements, community events, memes, live chat with staff, and more.',
     },
     {
-      url : 'http://classicffxi.wikia.com/wiki/Classic_FFXI_community',
-      image : 'https://avatars2.githubusercontent.com/u/1171011?s=200&v=4',
-      header : 'Eden Wiki',
-      description :
-          'This is a player-led wiki open for everyone to edit. Although it is specific to Eden, it may have incorrect information due to the fact that it is not an official source of information.',
+      url: 'http://classicffxi.wikia.com/wiki/Classic_FFXI_community',
+      image: 'https://avatars2.githubusercontent.com/u/1171011?s=200&v=4',
+      header: 'Eden Wiki',
+      description:
+        'This is a player-led wiki open for everyone to edit. Although it is specific to Eden, it may have incorrect information due to the fact that it is not an official source of information.',
     },
     {
-      url : 'https://github.com/EdenServer/community/issues',
-      image : 'https://static.ffxiah.com/images/icon/17001.png',
-      header : 'Bug Reports',
-      description :
-          'Eden has bugs too. :( See what issues we have and chime in if you found a bug!',
+      url: 'https://github.com/EdenServer/community/issues',
+      image: 'https://static.ffxiah.com/images/icon/17001.png',
+      header: 'Bug Reports',
+      description: 'Eden has bugs too. :( See what issues we have and chime in if you found a bug!',
     },
     {
-      url : 'https://github.com/EdenServer/eden-web/issues',
-      image : 'https://static.ffxiah.com/images/icon/17005.png',
-      header : 'Website Bugs',
-      description :
-          'In addition to technical bugs, Eden refines rules, information, and connection details occassionally. Chime in if you found an error!',
+      url: 'https://github.com/EdenServer/eden-web/issues',
+      image: 'https://static.ffxiah.com/images/icon/17005.png',
+      header: 'Website Bugs',
+      description: 'In addition to technical bugs, Eden refines rules, information, and connection details occassionally. Chime in if you found an error!',
     },
     {
-      url : 'https://ffxiclopedia.wikia.com/wiki/Main_Page',
-      image :
-          'https://vignette.wikia.nocookie.net/ffxi/images/b/b7/Favicon_Poroggo.png/revision/latest/scale-to-width-down/60?cb=20170308121952',
-      header : 'FFXIclopedia',
-      description :
-          'The defacto go-to resource of this era. Keep in mind that this site has been updated post-era and you may need to browse history of the page to get more accurate data.',
+      url: 'https://ffxiclopedia.wikia.com/wiki/Main_Page',
+      image: 'https://vignette.wikia.nocookie.net/ffxi/images/b/b7/Favicon_Poroggo.png/revision/latest/scale-to-width-down/60?cb=20170308121952',
+      header: 'FFXIclopedia',
+      description:
+        'The defacto go-to resource of this era. Keep in mind that this site has been updated post-era and you may need to browse history of the page to get more accurate data.',
     },
     {
-      url : 'https://www.bg-wiki.com/',
-      image : 'https://static.ffxiah.com/images/icon/15076.png',
-      header : 'BG Wikipedia',
-      description :
-          "Another resource, however more updated that the era wiki. Can be a good place to get information that isn't provided by other sources.",
+      url: 'https://www.bg-wiki.com/',
+      image: 'https://static.ffxiah.com/images/icon/15076.png',
+      header: 'BG Wikipedia',
+      description: "Another resource, however more updated that the era wiki. Can be a good place to get information that isn't provided by other sources.",
     },
     {
-      url : 'http://campsitarus.blogspot.com/',
-      image : 'https://www.bg-wiki.com/images/c/c3/Artemis%27s_Medal_icon.png',
-      header : 'Campsitarus',
-      description :
-          'A great resource on era retail camps. Some camps will not apply as they are WoTG area camps.',
+      url: 'http://campsitarus.blogspot.com/',
+      image: 'https://www.bg-wiki.com/images/c/c3/Artemis%27s_Medal_icon.png',
+      header: 'Campsitarus',
+      description: 'A great resource on era retail camps. Some camps will not apply as they are WoTG area camps.',
     },
     {
-      url : 'http://ffxi-atlas.mageringnetwork.com/',
-      image : 'https://static.ffxiah.com/images/icon/191.png',
-      header : 'FFXI Atlas (Mirror)',
-      description :
-          'All the maps without the cost of buying your own maps in game. However, good luck finding where you are in relation to the map!',
+      url: 'http://ffxi-atlas.mageringnetwork.com/',
+      image: 'https://static.ffxiah.com/images/icon/191.png',
+      header: 'FFXI Atlas (Mirror)',
+      description: 'All the maps without the cost of buying your own maps in game. However, good luck finding where you are in relation to the map!',
     },
     {
-      url : 'http://www.pyogenes.com/ffxi/timer/v2.html',
-      image : 'https://static.ffxiah.com/images/icon/3706.png',
-      header : 'Pyogenes Timer',
-      description :
-          "Pyogenes developed a Vana'Diel clock for retail. Eden runs on the same timer.",
+      url: 'http://www.pyogenes.com/ffxi/timer/v2.html',
+      image: 'https://static.ffxiah.com/images/icon/3706.png',
+      header: 'Pyogenes Timer',
+      description: "Pyogenes developed a Vana'Diel clock for retail. Eden runs on the same timer.",
     },
     {
-      url : 'http://www.ffxidb.com/',
-      image : 'http://www.ffxidb.com/public/img/xidb-logo-2.png',
-      header : 'FFXIDB',
-      description :
-          'Common site to track drop rates of items. Keep in mind that drop rates stay updated when people use their plugin. With things like TH10 on current retail this data may be inaccurate.',
+      url: 'http://www.ffxidb.com/',
+      image: 'http://www.ffxidb.com/public/img/xidb-logo-2.png',
+      header: 'FFXIDB',
+      description:
+        'Common site to track drop rates of items. Keep in mind that drop rates stay updated when people use their plugin. With things like TH10 on current retail this data may be inaccurate.',
     },
     {
-      url : 'http://ashita.atom0s.com/',
-      image : 'https://www.ashitaxi.com/assets/img/ashita.ico',
-      header : 'Ashita',
-      description :
-          'Eden officially supports Ashita v3 to enhance gameplay. Ashita v3 comes bundled with the Eden installer. You can find support for Ashita v3-specific things here.',
+      url: 'http://ashita.atom0s.com/',
+      image: 'https://www.ashitaxi.com/assets/img/ashita.ico',
+      header: 'Ashita',
+      description:
+        'Eden officially supports Ashita v3 to enhance gameplay. Ashita v3 comes bundled with the Eden installer. You can find support for Ashita v3-specific things here.',
     },
     {
-      url : 'http://windower.net/',
-      image : 'http://windower.net/img/icon.png',
-      header : 'Windower',
-      description :
-          'Windower 4 and 5 are not officially supported on Eden to enhance gameplay. While we do not offer setup or troubleshooting assistance, it does work on Eden.',
+      url: 'http://windower.net/',
+      image: 'http://windower.net/img/icon.png',
+      header: 'Windower',
+      description:
+        'Windower 4 and 5 are not officially supported on Eden to enhance gameplay. While we do not offer setup or troubleshooting assistance, it does work on Eden.',
     },
   ],
-  install : {
-    source1 : 'https://bit.ly/Eden535',
-    source2 : 'https://bit.ly/Eden534',
-    bootloader :
-        'https://github.com/EdenServer/xiloader/releases/latest/download/xiloader.exe',
-    discord : 'https://discord.gg/S3EAWr2Jec',
+  install: {
+    source1: 'https://bit.ly/Eden535',
+    source2: 'https://bit.ly/Eden534',
+    bootloader: 'https://github.com/EdenServer/xiloader/releases/latest/download/xiloader.exe',
+    discord: 'https://discord.gg/S3EAWr2Jec',
   },
 };
