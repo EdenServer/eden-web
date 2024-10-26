b4b843342e4db635351b7a4c7b5b3a861d9b7096
59blargedy <52636208+59blargedy@users.noreply.github.com>
2024-10-26 15:04:36 -0400

    some website text updates (#271)
    * updating text
    
    * news and links updates
    
    * fix prettier?

--- api/v1/config.js
+++ api/v1/config.js
@@ -16,10 +16,10 @@ module.exports = {
     rules: {
       updated: '3/25/2023',
       list: [
-        'Using a non-Eden bootloader--including using a modified version of the Eden bootloader--is NOT allowed. The bootloader must be downloaded from the official site or #annoucements channel in our official Discord otherwise there are no guarantees it is official.',
-        'If something feels like it is broken, either not like it was on era retail or a potential exploit or too overpowered (read: if something seems like it was not intentional from the development team) report it. Knowingly taking advantage of these activities is NOT allowed.',
+        'Using a non-Eden bootloader--including using a modified version of the Eden bootloader--is NOT allowed. The bootloader must be downloaded from the official site or #install-support-links channel in our official Discord otherwise there are no guarantees it is official.',
+        'If something feels like it is broken, either not like it was on era retail, or a potential exploit, or too overpowered (read: if something seems like it was not intentional from the development team) report it. Knowingly taking advantage of these activities is NOT allowed.',
         'Each player is only allowed to have 2 characters logged in and active, with an additional third character confined to a city and idle. This character may only be used to bazaar or mule items while your other characters are logged in. (This will not be locked down by IP, but it will be actively patrolled.) Tri-boxing 3 or more active characters is NOT allowed.',
-        'Claim bots, modifying dats to claim, any type of targeting using mob ID (such as /target 12345678) or using any other third party tools such as Shorthand or its Windower equivalent to claim is NOT allowed.',
+        'Claim bots, modifying DATs to claim, any type of targeting using mob ID (such as /target 12345678) or using any other third party tools such as Shorthand or its Windower equivalent to claim is NOT allowed.',
         "Any scripts, bots, or addons that automate actions are NOT allowed. Macros using the /wait or <wait> feature within the game are OK. (Such as /lastsynth macros up to the game's maximum of 6 lines.)",
         "Account sharing is strongly discouraged and may be disallowed in the future. If one or more of your accounts are linked to a player that is caught cheating by either logging into an offending player's account or by that player logging into one of your accounts you will share the same punishment as the cheater--which is 9 out of 10 times a permanent ban.",
         'Not really a rule but be aware that players may be limited in the future to 3 characters. So create new ones at your own risk. It is undecided how additional characters would be handled if we imposed these limits.',
@@ -34,7 +34,7 @@ module.exports = {
         'Harassment of any kind including offensive character or linkshell names is NOT allowed.',
         'Character names that depict cheating, could be considered sexually explicit, or any names that could be disallowed on retail are NOT allowed.',
         'Use of, or advertisement of, any cheat program/addon or a non-approved addon/program on Eden or its media is NOT allowed. There will be an allowed list and a non-allowed list. Those not in the approved list are discouraged from use as you may be penalized for using them. Use at your own risk. Those that are similar to a forbidden program are most likely forbidden as well.',
-        'GM interaction is limited here but if a GM messages you in game and you are active, respond within a reasonable time. Ignoring a GM is likely to get you jailed as we will likely think you are afk botting.',
+        'GM interaction is limited here but if a GM messages you in game and you are active, respond within a reasonable time. Ignoring a GM is likely to get you jailed as we will think you are afk botting.',
       ],
       violations: [
         {
@@ -118,24 +118,24 @@ module.exports = {
       ],
     },
     yells: {
-      updated: '7/12/2020',
+      updated: '10/12/2024',
       list: [
         'Yell is a global chat and has a 15 minute cooldown period. Shout is limited to a single zone with a large radius and has a 5 minute cooldown period, except in some instanced zones.',
-        'Zoning and logging off do not reset these cooldowns. New characters will have yell and shout cooldowns applied during the opening cutscene. Do not expect these limitations to change, as they are applied to prevent abuse.',
+        'Zoning and logging off do not reset these cooldowns. New characters will have yell and shout cooldowns applied during the opening cutscene.',
         'Do not yell or shout profanity, hate messages, or any harassing statements. Violating this rule may result in mutes and/or an account ban depending on severity.',
         'Do not banter back and forth between yourselves in yell (instead use shout for banter). This includes replying in yell to someone who has violated these rules. Banter in yell may result in mutes applied.',
-        'Asking a legitimate FFXI question in yell is fine. However, replies to questions should not be sent in yell. Shout or tell is preferred.',
+        'Asking a legitimate FFXI question in yell is fine. However, replies to questions should ideally be sent in tell.',
         'Adding on irrelevant banter not related to the purpose of a yell may result in a mute. Example: “WTS pebbles also... player XYZ sux!”',
       ],
     },
     search: {
       updated: '12/14/2020',
-      list: ['Do not use profanity, include hate messages or any harassing statements. These are public facing channels.'],
+      list: ['Do not use profanity, hate messages or any harassing statements. These are public-facing channels.'],
     },
     discord: {
       updated: '5/22/2020',
       list: [
-        "Please be respectful to each other. Consider this phrase when typing, treat others as you would want to be treated. You wouldn't want someone yelling at you or belittling you so you don't need to do it to another person.",
+        "Please be respectful to each other. Consider this phrase when typing - treat others as you would want to be treated. You wouldn't want someone yelling at you or belittling you so you don't need to do it to another person.",
         'Please keep the cursing at a minimum. Some people prefer not to read constant cursing. We know sometimes a word or two will slip out in the heat of the moment and that is ok but if it becomes a habit we will go to you in private about it.',
         'Do not post nudity of any kind.',
         'Do not give out any personal information in public. If you must send it do so via a PM. There are too many people that will take advantage of this personal information.',
@@ -152,11 +152,10 @@ module.exports = {
       description: 'Join us on Discord for technical support, up-to-date announcements, community events, memes, live chat with staff, and more.',
     },
     {
-      url: 'http://classicffxi.wikia.com/wiki/Classic_FFXI_community',
-      image: 'https://avatars2.githubusercontent.com/u/1171011?s=200&v=4',
+      url: 'https://edenxi.miraheze.org/wiki/Classic_FFXI_community',
+      image: 'https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg',
       header: 'Eden Wiki',
-      description:
-        'This is a player-led wiki open for everyone to edit. Although it is specific to Eden, it may have incorrect information due to the fact that it is not an official source of information.',
+      description: "This is a player-led wiki open for everyone to edit, intended to be specific to Eden's era.",
     },
     {
       url: 'https://github.com/EdenServer/community/issues',
@@ -174,14 +173,13 @@ module.exports = {
       url: 'https://ffxiclopedia.wikia.com/wiki/Main_Page',
       image: 'https://vignette.wikia.nocookie.net/ffxi/images/b/b7/Favicon_Poroggo.png/revision/latest/scale-to-width-down/60?cb=20170308121952',
       header: 'FFXIclopedia',
-      description:
-        'The defacto go-to resource of this era. Keep in mind that this site has been updated post-era and you may need to browse history of the page to get more accurate data.',
+      description: "The OG resource of era. You'll want to backdate any page you visit to get era-accurate data.",
     },
     {
       url: 'https://www.bg-wiki.com/',
       image: 'https://static.ffxiah.com/images/icon/15076.png',
       header: 'BG Wikipedia',
-      description: "Another resource, however more updated that the era wiki. Can be a good place to get information that isn't provided by other sources.",
+      description: "Another resource, heavily updated for the modern era. Can be a good place to get information that isn't provided by other sources.",
     },
     {
       url: 'http://campsitarus.blogspot.com/',
@@ -190,10 +188,10 @@ module.exports = {
       description: 'A great resource on era retail camps. Some camps will not apply as they are WoTG area camps.',
     },
     {
-      url: 'http://ffxi-atlas.mageringnetwork.com/',
+      url: 'https://wiki.ffo.jp/',
       image: 'https://static.ffxiah.com/images/icon/191.png',
-      header: 'FFXI Atlas (Mirror)',
-      description: 'All the maps without the cost of buying your own maps in game. However, good luck finding where you are in relation to the map!',
+      header: 'JP Wiki',
+      description: "This wiki offers another resource for data from the retail and 2008 eras. You'll need some form of translation if you can't read Japanese.",
     },
     {
       url: 'http://www.pyogenes.com/ffxi/timer/v2.html',
@@ -205,22 +203,21 @@ module.exports = {
       url: 'http://www.ffxidb.com/',
       image: 'http://www.ffxidb.com/public/img/xidb-logo-2.png',
       header: 'FFXIDB',
-      description:
-        'Common site to track drop rates of items. Keep in mind that drop rates stay updated when people use their plugin. With things like TH10 on current retail this data may be inaccurate.',
+      description: 'Common site to track drop rates of items. With things like drop rate changes and TH10 on retail, this data may not always be accurate.',
     },
     {
       url: 'http://ashita.atom0s.com/',
       image: 'https://www.ashitaxi.com/assets/img/ashita.ico',
       header: 'Ashita',
       description:
-        'Eden officially supports Ashita v3 to enhance gameplay. Ashita v3 comes bundled with the Eden installer. You can find support for Ashita v3-specific things here.',
+        'Eden supports Ashita v3 to enhance gameplay, and Ashita v3 comes bundled with the Eden installer. You can find support for Ashita v3-specific things here. v4 can be used with some tech know-how',
     },
     {
       url: 'http://windower.net/',
       image: 'http://windower.net/img/icon.png',
       header: 'Windower',
       description:
-        'Windower 4 and 5 are not officially supported on Eden to enhance gameplay. While we do not offer setup or troubleshooting assistance, it does work on Eden.',
+        'While we do not offer setup or troubleshooting assistance for Windower, Windower does work on Eden, and player tech support can help get you set up!',
     },
   ],
   install: {
