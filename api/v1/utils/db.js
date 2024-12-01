f9bf0999481d864d84354bd7966991371506b1e3
Scott Lassen <slassnpm@gmail.com>
2024-12-01 16:43:27 -0500

    prettier

--- api/v1/utils/db.js
+++ api/v1/utils/db.js
@@ -1,20 +1,22 @@
-module.exports = db => (statement, values = [], measure = true) =>
-  new Promise(async (resolve, reject) => {
-    const start = Date.now();
-    db.execute(statement, values, (error, value) => {
-      if (measure) {
-        const time = Date.now() - start;
-        if (time > 400) {
-          console.warn(`Query: ${statement}`);
-          console.warn(`Values: ${values}`);
-          console.warn(`Query took: ${time}ms`);
+module.exports =
+  db =>
+  (statement, values = [], measure = true) =>
+    new Promise(async (resolve, reject) => {
+      const start = Date.now();
+      db.execute(statement, values, (error, value) => {
+        if (measure) {
+          const time = Date.now() - start;
+          if (time > 400) {
+            console.warn(`Query: ${statement}`);
+            console.warn(`Values: ${values}`);
+            console.warn(`Query took: ${time}ms`);
+          }
         }
-      }
 
-      if (error) {
-        reject(error);
-      } else {
-        resolve(value);
-      }
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
     });
-  });
